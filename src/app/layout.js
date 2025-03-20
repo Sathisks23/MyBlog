97cba53c0867bd123364373e0e24f1642ae9bc53
Sathish <sathishksdckap@gmail.com>
2025-03-20 11:53:05 +0530

    seo verification added

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="uvZ3FIqlGxeTrYfKvjDBMVI980FIDsfUvG3Lj0lCvls" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
