71ac8880802131b0d774e5102c50b32fec8d87d0
Sathish <sathishksdckap@gmail.com>
2025-03-20 12:29:05 +0530

    seo verification added

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,18 +1,32 @@
 import { blogPosts } from "@/src/data/data";
 
 export async function GET() {
-    const baseUrl = process.env.BASE_URL||"https://sathish-blog.vercel.app/"
-    const urls = blogPosts
-    .map((post)=>`<url><loc>${baseUrl}/${post.slug}</loc></url>`)
-    .join('\n')
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url><loc>${baseUrl}</loc></url>
-      ${urls}
-    </urlset>`;
-    return new Response(sitemap, {
-        headers: {
-          "Content-Type": "application/xml",
-        },
-      });
+  const baseUrl = process.env.BASE_URL || "https://sathish-blog.vercel.app/"; 
+  const urls = blogPosts
+    .map((post) => `
+      <url>
+        <loc>${baseUrl}/${post.slug}</loc>
+        <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+    `)
+    .join("\n");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${baseUrl}</loc>
+      <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+    ${urls}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
